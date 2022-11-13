cfaea75e5af6553efc8a202870e0917ae2cabe69
furkangergin <hunter_1325@hotmail.com>
2022-11-14 00:16:45 +0300

    furkan with initials

--- src/test/java/com/Xfleet/step_definitions/FilterMenu_StepDefinitions.java
+++ src/test/java/com/Xfleet/step_definitions/FilterMenu_StepDefinitions.java
@@ -16,18 +16,18 @@ public class FilterMenu_StepDefinitions {
     LoginPage loginPage = new LoginPage();
     FilterMenuPage filterMenuPage = new FilterMenuPage();
 
-    @When("The user enters credentials as a {string} and click login button")
-    public void FLTAP_1016the_user_enters_credentials_as_a_and_click_login_button(String usertype) {
+    @When("MFGthe user enters credentials as a {string} and click login button")
+    public void MFGthe_user_enters_credentials_as_a_and_click_login_button(String usertype) {
         loginPage.loginUserType(usertype);
         filterMenuPage.waitUntilVisibleWait(filterMenuPage.loaderMask);
     }
-    @When("The user clicks on the vehicles under the fleet")
-    public void FLTAP_1016click_on_the_vehicles_under_the_fleet() {
+    @When("MFGthe user clicks on the vehicles under the fleet")
+    public void MFGclick_on_the_vehicles_under_the_fleet() {
         filterMenuPage.navigateToModule("Fleet","Vehicles");
         BrowserUtils.sleep(3);
     }
-    @When("The user clicks on the filter icon")
-    public void FLTAP_1016click_on_the_filter_icon() {
+    @When("MFGthe user clicks on the filter icon")
+    public void MFGclick_on_the_filter_icon() {
         String filterIcon="//a[@title='Filters']";
         //filterMenuPage.filterIcon.click();
         try {
@@ -39,43 +39,43 @@ public class FilterMenu_StepDefinitions {
             BrowserUtils.clickWithTimeOut(Driver.getDriver().findElement(By.xpath(filterIcon)),  5);
         }
     }
-    @Then("The Manage Filter button should be visible")
-    public void FLTAP_1016the_manage_filter_button_should_be_visible() {
+    @Then("MFGthe Manage Filter button should be visible")
+    public void MFGthe_manage_filter_button_should_be_visible() {
         Assert.assertTrue(filterMenuPage.manageFilters.isDisplayed());
     }
-    @When("The user clicks on the Manage Filters menu")
-    public void FLTAP_1016click_on_the_manage_filters_menu() {
+    @When("MFGthe user clicks on the Manage Filters menu")
+    public void MFGclick_on_the_manage_filters_menu() {
         BrowserUtils.sleep(3);
         filterMenuPage.manageFilters.click();
     }
-    @When("The user clicks on the Tags")
-    public void FLTAP_1016click_on_the_tags() {
+    @When("MFGthe user clicks on the Tags")
+    public void MFGclick_on_the_tags() {
         BrowserUtils.waitFor(3);
         filterMenuPage.tags.click();
             }
-    @When("The user enters credentials as a store manager and click login button")
-    public void FLTAP_1016the_user_enters_credentials_as_a_store_manager_and_click_login_button() {
+    @When("MFGthe user enters credentials as a store manager and click login button")
+    public void MFGthe_user_enters_credentials_as_a_store_manager_and_click_login_button() {
         loginPage.username.sendKeys(ConfigurafionReader.getProperty("store_manager_username") + Keys.ENTER);
         loginPage.password.sendKeys(ConfigurafionReader.getProperty("store_manager_password"));
         loginPage.loginBtn.click();
     }
-    @Then("Tags should be selected")
-    public void FLTAP_1016tags_should_be_selected() {
+    @Then("MFGtags should be selected")
+    public void MFGtags_should_be_selected() {
         Assert.assertTrue(filterMenuPage.tags.isSelected());
     }
-    @When("The user enters credentials as a sales manager and click login button")
-    public void FLTAP_1016the_user_enters_credentials_as_a_sales_manager_and_click_login_button() {
+    @When("MFGthe user enters credentials as a sales manager and click login button")
+    public void MFGthe_user_enters_credentials_as_a_sales_manager_and_click_login_button() {
         loginPage.username.sendKeys(ConfigurafionReader.getProperty("sales_manager_username") + Keys.ENTER);
         loginPage.password.sendKeys(ConfigurafionReader.getProperty("sales_manager_password"));
         loginPage.loginBtn.click();
     }
-    @When("The user types the filter name")
-    public void FLTAP_1016the_user_types_the_filter_name() {
+    @When("MFGthe user types the filter name")
+    public void MFGthe_user_types_the_filter_name() {
         filterMenuPage.searchManage.sendKeys("Tags");
         BrowserUtils.waitFor(3);
     }
-    @Then("The user should be apply filters")
-    public void FLTAP_1016the_user_should_be_apply_filters() {
+    @Then("MFGthe user should be apply filters")
+    public void MFGthe_user_should_be_apply_filters() {
         String actual = filterMenuPage.typeTags.getText();
         Assert.assertEquals("Tags",actual);
     }
--- src/test/resources/features/FilterMenu.feature
+++ src/test/resources/features/FilterMenu.feature
@@ -5,10 +5,10 @@ Feature: Store manager and sales manager should be able to filter menu
 
   @FLTAP-1052
   Scenario Outline: When a user clicks on the filter icon, the "Manage Filter" button should be visible
-   When The user enters credentials as a "<usertype>" and click login button
-   And The user clicks on the vehicles under the fleet
-   And The user clicks on the filter icon
-   Then The Manage Filter button should be visible
+   When MFGthe user enters credentials as a "<usertype>" and click login button
+   And MFGthe user clicks on the vehicles under the fleet
+   And MFGthe user clicks on the filter icon
+   Then MFGthe Manage Filter button should be visible
 
     Examples:
       | usertype       |
@@ -17,12 +17,12 @@ Feature: Store manager and sales manager should be able to filter menu
 
   @FLTAP-1073
   Scenario Outline: User can apply filters by clicking on the filter name, from the 'Manage Filters' menu.
-    When The user enters credentials as a "<usertype>" and click login button
-    And The user clicks on the vehicles under the fleet
-    And The user clicks on the filter icon
-    And The user clicks on the Manage Filters menu
-    And The user clicks on the Tags
-    Then Tags should be selected
+    When MFGthe user enters credentials as a "<usertype>" and click login button
+    And MFGthe user clicks on the vehicles under the fleet
+    And MFGthe user clicks on the filter icon
+    And MFGthe user clicks on the Manage Filters menu
+    And MFGthe user clicks on the Tags
+    Then MFGtags should be selected
 
     Examples:
       | usertype       |
@@ -31,12 +31,12 @@ Feature: Store manager and sales manager should be able to filter menu
 
   @FLTAP-1075
   Scenario Outline:  User can apply filters by typing the filter name, from the 'Manage Filters' menu.
-    When The user enters credentials as a "<usertype>" and click login button
-    And The user clicks on the vehicles under the fleet
-    And The user clicks on the filter icon
-    And The user clicks on the Manage Filters menu
-    And The user types the filter name
-    Then The user should be apply filters
+    When MFGthe user enters credentials as a "<usertype>" and click login button
+    And MFGthe user clicks on the vehicles under the fleet
+    And MFGthe user clicks on the filter icon
+    And MFGthe user clicks on the Manage Filters menu
+    And MFGthe user types the filter name
+    Then MFGthe user should be apply filters
 
     Examples:
       | usertype       |
