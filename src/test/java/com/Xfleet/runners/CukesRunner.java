c1b0c62bd324bd9af9ddfb2060ccbcf2ad419924
akyuz-suleyman <akyuz.s3535@gmail.com>
2022-11-16 20:43:23 +0300

    Updateded

--- src/test/java/com/Xfleet/runners/CukesRunner.java
+++ src/test/java/com/Xfleet/runners/CukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/Xfleet/step_definitions",
         dryRun = false,
-        tags="@FLTAP-1018"
+        tags=""
 )
 public class CukesRunner {
 
